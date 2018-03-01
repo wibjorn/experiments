bd522dad4c7c782034ed359ae9962976e5fa4551
Will Bjorn <wibjorn@microsoft.com>
2018-03-01 13:45:43 -0800

    update replace function.

--- replace-all.js
+++ replace-all.js
@@ -1,21 +1,24 @@
 const replace = require('replace-in-file');
 
-const folder = process.argv[2] || '.'
+const folder = process.argv[2] || '.';
+const prefix = process.argv[3] || '';
 
 const options = {
 
     //Glob(s) 
     files: [
-        `${folder}/**/*.md`
+        `${folder}/**/${prefix}*.md`
     ],
 
     //Replacement to make (string or regex) 
-    from: /(---\s*?#\s{1}\S*)([s|\n|\r\n]*#\s{1}\S*)/,
+    from: /(---\s*?#\s{1}[^\n]*)(\s*?#\s{1}[^\n]*)/,
     to: '$1',
 };
 
 if (process.argv[2]) {
     console.log(`Searching and ${process.argv[2]} in current folder and its children.`);
+    if (process.argv[3])
+        console.log(`Using prefix ${prefix} to check only files that begin with ${prefix}`);
 } else {
     console.log(`Nothing passed in, searching and replacing in current folder and its children`)
 }
@@ -23,6 +26,7 @@ if (process.argv[2]) {
 replace(options)
     .then(changes => {
         console.log('Modified files:', changes.join(',\n'));
+        console.log(changes.length + ' files modified');
     })
     .catch(error => {
         console.error('Error occurred:', error);
